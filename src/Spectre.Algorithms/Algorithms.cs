ad3770976dedbd879613fee55d10d509cfff87b5
Michał 'Sand3r' Gallus <sand3r@interia.eu>
2017-02-22 21:02:23 +0100

    Created project, added base for main file

---
+++ src/Spectre.Algorithms/Algorithms.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatlabAlgorithmsNative;
+
+namespace Spectre.Algorithms
+{
+    public class Algorithms
+    {
+        private Gmm gaussianMixtureModel;
+        private Preprocessing preprocessing;
+        private Segmentation segmentation;
+
+        public Algorithms()
+        {
+            gaussianMixtureModel = new Gmm();
+            preprocessing = new Preprocessing();
+            segmentation = new Segmentation();
+        }
+
+        public void ApplyGmm()
+        {
+            
+        }
+
+        public object[] EstimateGmm()
+        {
+            return null;
+        }
+
+        public void PeakAlignmentFFT()
+        {
+            
+        }
+
+        public void RemoveBaseline()
+        {
+            
+        }
+
+        public void TicNorm()
+        {
+            
+        }
+
+        public object[] Divik()
+        {
+            return null;
+        }
+    }
+}
