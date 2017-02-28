141cfd75dc9cce8428da3d2a5708725f2ffda006
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-02-28 15:15:00 +0100

    Added exclusion of tests into test running script

--- scripts/run_tests.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-echo Test discovery started...
-dir *.tests.dll /b /s | findstr /v obj > __tmp.txt
-set dirs=
-FOR /F %%i IN (__tmp.txt) DO set dirs=!dirs! %%i
-del __tmp.txt
-echo Tests found: %dirs%
-vstest.console /logger:Appveyor %dirs% /Platform:x64 /inIsolation
