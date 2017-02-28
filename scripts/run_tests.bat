7a01c651782b345edf38a6701e694bbab52abe71
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-02-28 09:15:41 +0100

    Fixed current directory changes during build

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,11 +1,9 @@
+@echo off
 setlocal EnableDelayedExpansion
 echo Test discovery started...
 dir *.tests.dll /b /s | findstr /v obj > __tmp.txt
-echo Saved
 set dirs=
-echo Iterating
 FOR /F %%i IN (__tmp.txt) DO set dirs=!dirs! %%i
-echo Removing tmp
 del __tmp.txt
 echo Tests found: %dirs%
 vstest.console /logger:Appveyor %dirs% /Platform:x64
