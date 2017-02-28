b9ac6c2427d819370385b9f6f0590fc82a1ddec5
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-02-28 09:04:16 +0100

    Verbosity @test

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,8 +1,9 @@
-@echo off
+REM @echo off
 setlocal EnableDelayedExpansion
+echo Test discovery started...
 dir *.tests.dll /b /s | findstr /v obj > __tmp.txt
 set dirs=
 FOR /F %%i IN (__tmp.txt) DO set dirs=!dirs! %%i
 del __tmp.txt
-echo Running tests: %dirs%
+echo Tests found: %dirs%
 vstest.console /logger:Appveyor %dirs% /Platform:x64
