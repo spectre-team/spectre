4febc55121483d67bd384ebfbcd2f481bfdb04ee
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-02-28 10:32:50 +0100

    MCR install script waits for installation to end

--- scripts/install_mcr.ps1
+++ scripts/install_mcr.ps1
@@ -1,2 +1,7 @@
+Write-Host "Gathering MCR installer..." -ForegroundColor Yellow
 (new-object net.webclient).DownloadFile('https://www.mathworks.com/supportfiles/downloads/R2016b/deployment_files/R2016b/installers/win64/MCR_R2016b_win64_installer.exe', 'mcr_setup.exe')
-mcr_setup.exe -mode silent -agreeToLicense yes
+Write-Host "MCR installer downloaded. Installation..." -ForegroundColor Yellow
+$job = Start-Job { mcr_setup.exe -mode silent -agreeToLicense yes }
+Wait-Job $job
+Receive-Job $job
+Write-Host "MCR installed." -ForegroundColor Yellow
