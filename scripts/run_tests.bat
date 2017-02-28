548de57e60a728cc2ae12f89f6d457940fbacfa4
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-02-28 09:10:08 +0100

    More verbose

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,9 +1,11 @@
-REM @echo off
 setlocal EnableDelayedExpansion
 echo Test discovery started...
 dir *.tests.dll /b /s | findstr /v obj > __tmp.txt
+echo Saved
 set dirs=
+echo Iterating
 FOR /F %%i IN (__tmp.txt) DO set dirs=!dirs! %%i
+echo Removing tmp
 del __tmp.txt
 echo Tests found: %dirs%
 vstest.console /logger:Appveyor %dirs% /Platform:x64
