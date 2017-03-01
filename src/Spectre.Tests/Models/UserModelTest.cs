19e40d4390dab37e7a3d1c896ee617e2c7479b2d
Regis128 <regis1286@gmail.com>
2017-03-01 14:25:55 +0100

    remove test temporarly

--- src/Spectre.Tests/Models/UserModelTest.cs
+++ src/Spectre.Tests/Models/UserModelTest.cs
@@ -8,37 +8,4 @@ using System.Threading.Tasks;
 
 namespace Spectre.Tests.Models
 {
-    [TestClass()]
-    public class UserModelTest
-    {
-        [TestMethod]
-        public void AddUser()
-        {
-            using (var db = new AuthContext())
-            {
-                var username = "test";
-                var password = "haslo";
-                var password2 = "haslo";
-
-                if (password == password2)
-                {
-                    var user = new UserModel(username, password);
-                    db.Users.Add(user);
-                    db.SaveChanges();
-                }
-                
-                var query = from b in db.Users
-                            orderby b.username
-                            select b;
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
-                {
-                    Console.WriteLine(item.username);
-                }
-
-                Assert.AreEqual("2", "2");
-            }
-        }
-    }
 }
