a3fcb6ec27f018cf8a07ca8b338743586a8b77c9
Regis128 <regis1286@gmail.com>
2017-03-01 14:25:23 +0100

    UserModel + test

---
+++ src/Spectre.Tests/Models/UserModelTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spectre.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectre.Tests.Models
+{
+    [TestClass()]
+    public class UserModelTest
+    {
+        [TestMethod]
+        public void AddUser()
+        {
+            using (var db = new AuthContext())
+            {
+                var username = "test";
+                var password = "haslo";
+                var password2 = "haslo";
+
+                if (password == password2)
+                {
+                    var user = new UserModel(username, password);
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+                
+                var query = from b in db.Users
+                            orderby b.username
+                            select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.username);
+                }
+
+                Assert.AreEqual("2", "2");
+            }
+        }
+    }
+}
---
+++ src/Spectre/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Spectre.Models;
+
+namespace Spectre.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public virtual List<UserModel> Users { get; set; }
+
+        public UserModel(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
+
+public class AuthContext : DbContext
+{
+    public DbSet<UserModel> Users { get; set; }
+}
