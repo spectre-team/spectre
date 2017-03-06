10babd8e7cbadb629e5c9c40f26657bdb547257b
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-07 00:31:02 +0100

    Added license headers

--- src/Spectre.Algorithms.Tests/AlgorithmsTests.cs
+++ src/Spectre.Algorithms.Tests/AlgorithmsTests.cs
@@ -1,14 +1,30 @@
-﻿using NUnit.Framework;
-using Spectre.Algorithms;
+﻿/*
+ * AlgorithmsTests.cs
+ * Checks, whether MCR is properly called and result may be obtained.
+ * 
+   Copyright 2017 Wilgierz Wojciech, Grzegorz Mrukwa
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using NUnit.Framework;
 using System;
-using System.Net.Configuration;
 
 namespace Spectre.Algorithms.Tests
 {
 	[TestFixture, Category("Algorithm")]
 	public class AlgorithmsTests
 	{
-		Spectre.Algorithms.Algorithms alg;
+		Algorithms alg;
 
 		[OneTimeSetUp]
 		public void SetUpClass()
