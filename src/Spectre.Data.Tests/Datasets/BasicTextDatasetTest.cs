e2a743454a65ea589edf58aa579de50019491ca3
Dariusz Kuchta <daroslawowicz@gmail.com>
2017-03-15 20:27:37 +0100

    Starting testing

--- src/Spectre.Data.Tests/Datasets/BasicTextDatasetTest.cs
+++ src/Spectre.Data.Tests/Datasets/BasicTextDatasetTest.cs
@@ -4,9 +4,56 @@ using Spectre.Data.Datasets;
 
 namespace Spectre.Data.Tests.Datasets
 {
-    [TestFixture]
+    [TestFixture, Category("Dataset")]
     public class BasicTextDatasetTest
     {
+        private IDataset _dataset;
 
+        [Test]
+        public void CreateFromFileTest()
+        {
+            try
+            {
+                _dataset = new BasicTextDataset("small-test.txt");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Dataset failed to initialize from file.", e);
+            }
+        }
+
+        public void CreateFromRawDataTest()
+        {
+            double[] mz = null;
+            double[,] data = null;
+
+            try
+            {
+                _dataset = new BasicTextDataset(mz, data);
+                Assert.Fail("Dataset accepted empty input.");
+            }
+            catch (Exception) { }
+
+            mz = new[] {1.0, 2.0, 3.0};
+            data = new[,] {{1.0, 2.0}};
+
+            try
+            {
+                _dataset = new BasicTextDataset(mz, data);
+                Assert.Fail("Dataset accepted data of different lengths.");
+            }
+            catch (Exception) { }
+
+            data = new[,] { { 1, 1.1, 1.2 }, { 1, 1.1, 1.2 }, { 1, 1.1, 1.2 } };
+
+            try
+            {
+                _dataset = new BasicTextDataset(mz, data);
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Dataset failed to initialize from correct raw data.");
+            }
+        }
     }
 }
