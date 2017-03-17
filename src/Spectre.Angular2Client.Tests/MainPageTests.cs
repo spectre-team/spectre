4890a3d7a1df444041d83085fb49cbd697850fd0
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-17 15:29:02 +0100

    Adds test to Angular2Client
    Simple Selenium tests to verify if everything works correctly.

---
+++ src/Spectre.Angular2Client.Tests/MainPageTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+
+namespace Spectre.Angular2Client.Tests
+{
+    [TestFixture, Category("Angular2Client")]
+    public class MainPageTests
+    {
+        private IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void SetUpClass()
+        {
+            _driver = new InternetExplorerDriver {Url = "http://localhost:440"};
+        }
+
+        [OneTimeTearDown]
+        public void TearDownClass()
+        {
+            _driver.Close();
+            _driver.Dispose();
+            _driver = null;
+        }
+
+        [Test]
+        public void PageOpens()
+        {
+            Assert.IsTrue(_driver.Title.Contains("Spectre"), $"Title does not contain 'Spectre': {_driver.Title}\nSource:\n{_driver.PageSource}");
+        }
+
+        [Test]
+        public void DriverStarts()
+        {
+            //do nothing
+        }
+    }
+}
