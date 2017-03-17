a567c68397e88f837170dbed85e5abc946ad7a63
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-17 11:52:42 +0100

    Add missing script
    Adds missing script to make it possible to run testing through IIS

---
+++ scripts/BindPageToIIS.ps1
@@ -0,0 +1,5 @@
+Import-Module "WebAdministration"
+# New-Item 'IIS:\Sites\Default Web Site\spectre' -physicalPath 'c:\projects\spectre\src\Spectre.Angular2Client' -type Application
+New-Item iis:Sites\Spectre -bindings @{protocol="https";bindingInformation=":440"} -physicalPath c:\projects\spectre\src\Spectre.Angular2Client\dist | Out-Null
+# spawn IIS worker
+Invoke-WebRequest https://localhost:440
