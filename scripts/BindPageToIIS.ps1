51cdf94a7fdbf271813533f6c3ad3d72f95798f8
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-17 14:57:51 +0100

    Resolve IIS hosting problem
    Fixes script which binds page to IIS, so we can debug and test it.

--- scripts/BindPageToIIS.ps1
+++ scripts/BindPageToIIS.ps1
@@ -1,5 +1,4 @@
 Import-Module "WebAdministration"
-# New-Item 'IIS:\Sites\Default Web Site\spectre' -physicalPath 'c:\projects\spectre\src\Spectre.Angular2Client' -type Application
-New-Item iis:Sites\Spectre -bindings @{protocol="https";bindingInformation=":440"} -physicalPath c:\projects\spectre\src\Spectre.Angular2Client\dist | Out-Null
+New-Item iis:Sites\Spectre -bindings @{protocol="http";bindingInformation="*:440:"} -physicalPath c:\projects\spectre\src\Spectre.Angular2Client\dist | Out-Null
 # spawn IIS worker
 Invoke-WebRequest https://localhost:440 -TimeoutSec 120
