ac63de6ce0dba03c860069e60551854b2ab31791
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-17 12:53:23 +0100

    Add 2min timeout to IIS bind
    Possibly necessary to query this page from powershell

--- scripts/BindPageToIIS.ps1
+++ scripts/BindPageToIIS.ps1
@@ -2,4 +2,4 @@ Import-Module "WebAdministration"
 # New-Item 'IIS:\Sites\Default Web Site\spectre' -physicalPath 'c:\projects\spectre\src\Spectre.Angular2Client' -type Application
 New-Item iis:Sites\Spectre -bindings @{protocol="https";bindingInformation=":440"} -physicalPath c:\projects\spectre\src\Spectre.Angular2Client\dist | Out-Null
 # spawn IIS worker
-Invoke-WebRequest https://localhost:440
+Invoke-WebRequest https://localhost:440 -TimeoutSec 120
