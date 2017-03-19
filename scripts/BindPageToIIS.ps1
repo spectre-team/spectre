f87f39bde21806bf0306691ee450d35703ba8b4c
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-19 23:09:25 +0100

    Removes unnecessary IIS binding
    Part of migration to JS e2e testing.

--- scripts/BindPageToIIS.ps1
+++
@@ -1,4 +0,0 @@
-Import-Module "WebAdministration"
-New-Item iis:Sites\Spectre -bindings @{protocol="http";bindingInformation="*:440:"} -physicalPath c:\projects\spectre\src\Spectre.Angular2Client\dist | Out-Null
-# spawn IIS worker
-Invoke-WebRequest http://localhost:440 -TimeoutSec 120 | Out-Null
