698e7770349e464650e565914bfde3959164872d
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-19 22:20:28 +0100

    Fix Win7 issue through .NET
    Workarounds missing cmdlet.

--- scripts/InstallNodeDev.ps1
+++ scripts/InstallNodeDev.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-Invoke-WebRequest https://nodejs.org/dist/v6.10.0/node-v6.10.0-x64.msi -OutFile node.msi
+(New-Object Net.WebClient).DownloadFile('https://nodejs.org/dist/v6.10.0/node-v6.10.0-x64.msi', 'node.msi')
 Start-Process msiexec.exe -ArgumentList "/i node.msi /quiet" -Wait | Wait-Process
 Remove-Item node.msi
 $env:Path = $env:Path + ";C:\Program Files\nodejs"
