4486d526981d431cc7ea594adba9f0cbf88461fe
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-21 02:12:25 +0100

    Add WPF project for DiviK
    Creates project with sample VM and referenced framework for MVVM.

---
+++ src/Spectre.DivikWpfClient/ViewModel/MainPageVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Mvvm.Base;
+
+namespace Spectre.DivikWpfClient.ViewModel
+{
+    /// <summary>
+    /// Serves as VM for main page.
+    /// </summary>
+    /// <seealso cref="Spectre.Mvvm.Base.PropertyChangedNotification" />
+    public class MainPageVm: PropertyChangedNotification
+    {
+        public string InputPath
+        {
+            get { return GetValue(() => InputPath); }
+            set { SetValue(() => InputPath, value); }
+        }
+    }
+}
