cc576a2dfaaad9091bcdab7f034db3b40099c1af
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-03-21 01:51:55 +0100

    Add simple MVVM framework
    Allows to create simple WPF application in MVVM pattern.

---
+++ src/Spectre.Mvvm.Tests/Converters/AnyConverterThrowTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using NUnit.Framework;
+using Spectre.Mvvm.Converters;
+
+namespace Spectre.Mvvm.Tests.Converters
+{
+    [TestFixture]
+    public class AnyConverterThrowTests
+    {
+        public static IEnumerable<ConverterTestCase> TwoWayConverters
+        {
+            get
+            {
+                yield return new ConverterTestCase(new BoolToVisibilityConverter(), typeof(Visibility), typeof(bool));
+                yield return new ConverterTestCase(new CombinedConverter(), typeof(int), typeof(bool));
+                yield return new ConverterTestCase(new InverseBoolConverter(), typeof(bool), typeof(bool));
+            }
+        }
+
+        public static IEnumerable<ConverterTestCase> ForwardOnlyConverters
+        {
+            get
+            {
+                yield return new ConverterTestCase(new IntPositivenessToBoolConverter(), typeof(int), typeof(bool));
+            }
+        }
+
+        public static IEnumerable<ConverterTestCase> AllConverters
+        {
+            get
+            {
+                foreach (var tc in TwoWayConverters)
+                    yield return tc;
+                foreach (var tc in ForwardOnlyConverters)
+                    yield return tc;
+            }
+        }
+
+        [Test, TestCaseSource("AllConverters")]
+        public virtual void ThrowsOnNullArgumentTest(ConverterTestCase convCase)
+        {
+            Assert.Throws<NullReferenceException>(
+                () => convCase.Converter.Convert(null, convCase.GuiType, null, CultureInfo.CurrentCulture),
+                "Converted null without an exception.");
+        }
+
+        [Test, TestCaseSource("TwoWayConverters")]
+        public virtual void ThrowsOnNullArgumentBackTest(ConverterTestCase convCase)
+        {
+            Assert.Throws<NullReferenceException>(
+                () => convCase.Converter.ConvertBack(null, convCase.BackendType, null, CultureInfo.CurrentCulture),
+                "Converted back null without an exception.");
+        }
+
+        public class ConverterTestCase
+        {
+            public IValueConverter Converter { get; }
+            public Type GuiType { get; }
+            public Type BackendType { get; }
+
+            public ConverterTestCase(IValueConverter converter, Type gui, Type backend)
+            {
+                Converter = converter;
+                GuiType = gui;
+                BackendType = backend;
+            }
+        }
+    }
+}
---
+++ src/Spectre.Mvvm/Converters/CombinedConverter.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * CombinedConverter.cs
+ * Converter which allows chaining of other converters.
+ * 
+   Copyright 2017 Grzegorz Mrukwa
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
+using System.Windows.Data;
+
+namespace Spectre.Mvvm.Converters
+{
+    /// <summary>
+	/// Converter allowing nesting of the converters.
+	/// </summary>
+	public class CombinedConverter : IValueConverter
+    {
+        /// <summary>
+        /// Gets or sets the first converter used.
+        /// </summary>
+        /// <value>
+        /// The first converter.
+        /// </value>
+        public IValueConverter First { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second converter used.
+        /// </summary>
+        /// <value>
+        /// The second converter.
+        /// </value>
+        public IValueConverter Second { get; set; }
+
+        /// <summary>
+        /// Modifies the source data before passing it to the target for display in the UI.
+        /// </summary>
+        /// <param name="value">The source data being passed to the target.</param>
+        /// <param name="targetType">The <see cref="T:System.Type" /> of data expected by the target dependency property.</param>
+        /// <param name="parameter">An optional parameter to be used in the converter logic.</param>
+        /// <param name="culture">The culture of the conversion.</param>
+        /// <returns>
+        /// The value to be passed to the target dependency property.
+        /// </returns>
+        /// <exception cref="NullReferenceException">if value is null</exception>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Second.Convert(First.Convert(value, targetType, parameter, culture), targetType, parameter, culture);
+        }
+
+        /// <summary>
+        /// Modifies the target data before passing it to the source object.  This method is called only in <see cref="F:System.Windows.Data.BindingMode.TwoWay" /> bindings.
+        /// </summary>
+        /// <param name="value">The target data being passed to the source.</param>
+        /// <param name="targetType">The <see cref="T:System.Type" /> of data expected by the source object.</param>
+        /// <param name="parameter">An optional parameter to be used in the converter logic.</param>
+        /// <param name="culture">The culture of the conversion.</param>
+        /// <returns>
+        /// The value to be passed to the source object.
+        /// </returns>
+        /// <exception cref="NullReferenceException">if value is null</exception>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return First.ConvertBack(Second.ConvertBack(value, targetType, parameter, culture), targetType, parameter, culture);
+        }
+    }
+}
