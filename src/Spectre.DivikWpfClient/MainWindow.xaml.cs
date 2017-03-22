defea217e3ad428b2ad13a2569b536f123f387b2
Michał Wolny <michalwoln@gmail.com>
2017-03-22 10:09:21 +0100

    feature/wpf-client demo main window added, viewmodel filled in

--- src/Spectre.DivikWpfClient/MainWindow.xaml.cs
+++ src/Spectre.DivikWpfClient/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,45 @@ namespace Spectre.DivikWpfClient
         public MainWindow()
         {
             InitializeComponent();
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            MaxKNumberTextBox.Text = 10.ToString();
+            LevelNumberTextBox.Text = 3.ToString();
+            UsingLevelsCheckbox.IsChecked = true;
+            UsingAmplitudeFiltrationCheckbox.IsChecked = true;
+            UsingVarianceFiltrationCheckbox.IsChecked = true;
+            PercentSizeLimitTextBox.Text = 0.001.ToString();
+            FeaturePreservationLimitTextBox.Text = 0.05.ToString();
+            //MetricComboBox
+            PlottingPartitionsCheckbox.IsChecked = false;
+            PlottingRecursivelyCheckbox.IsChecked = false;
+            PlottingDecompositionCheckbox.IsChecked = false;
+            PlottingDecompositionRecursivelyCheckbox.IsChecked = false;
+            MaxComponentsForDecompositionNumberTextBox.Text = 3.ToString();
+            OutputPathTextBox.Text = ".";
+            CachePathTextBox.Text = ".";
+            CachingCheckbox.IsChecked = false;
+            VerboseCheckbox.IsChecked = false;
+            KmeansMaxItersNumberTextBox.Text = 100.ToString();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- src/Spectre.DivikWpfClient/ViewModel/MainPageVm.cs
+++ src/Spectre.DivikWpfClient/ViewModel/MainPageVm.cs
@@ -18,5 +18,109 @@ namespace Spectre.DivikWpfClient.ViewModel
             get { return GetValue(() => InputPath); }
             set { SetValue(() => InputPath, value); }
         }
+
+        public int MaxK
+        {
+            get { return GetValue(() => MaxK); }
+            set { SetValue(() => MaxK, value); }
+        }
+
+        public int Level
+        {
+            get { return GetValue(() => Level); }
+            set { SetValue(() => Level, value); }
+        }
+
+        public bool UsingLevels
+        {
+            get { return GetValue(() => UsingLevels); }
+            set { SetValue(() => UsingLevels, value); }
+        }
+
+        public bool UsingAmplitudeFiltration
+        {
+            get { return GetValue(() => UsingAmplitudeFiltration); }
+            set { SetValue(() => UsingAmplitudeFiltration, value); }
+        }
+
+        public bool UsingVarianceFiltration
+        {
+            get { return GetValue(() => UsingVarianceFiltration); }
+            set { SetValue(() => UsingVarianceFiltration, value); }
+        }
+
+        public double PercentSizeLimit
+        {
+            get { return GetValue(() => PercentSizeLimit); }
+            set { SetValue(() => PercentSizeLimit, value); }
+        }
+
+        public double FeaturePreservationLimit
+        {
+            get { return GetValue(() => FeaturePreservationLimit); }
+            set { SetValue(() => FeaturePreservationLimit, value); }
+        }
+
+        //metric 
+
+        public bool PlottingPartitions
+        {
+            get { return GetValue(() => PlottingPartitions); }
+            set { SetValue(() => PlottingPartitions, value); }
+        }
+
+        public bool PlottingRecursively
+        {
+            get { return GetValue(() => PlottingRecursively); }
+            set { SetValue(() => PlottingRecursively, value); }
+        }
+
+        public bool PlottingDecomposition
+        {
+            get { return GetValue(() => PlottingDecomposition); }
+            set { SetValue(() => PlottingDecomposition, value); }
+        }
+
+        public bool PlottingDecompositionRecursively
+        {
+            get { return GetValue(() => PlottingDecompositionRecursively); }
+            set { SetValue(() => PlottingDecompositionRecursively, value); }
+        }
+
+        public int MaxComponentsForDecomposition
+        {
+            get { return GetValue(() => MaxComponentsForDecomposition); }
+            set { SetValue(() => MaxComponentsForDecomposition, value); }
+        }
+
+        public string OutputPath
+        {
+            get { return GetValue(() => OutputPath); }
+            set { SetValue(() => OutputPath, value); }
+        }
+
+        public string CachePath
+        {
+            get { return GetValue(() => CachePath); }
+            set { SetValue(() => CachePath, value); }
+        }
+
+        public bool Caching
+        {
+            get { return GetValue(() => Caching); }
+            set { SetValue(() => Caching, value); }
+        }
+
+        public bool Verbose
+        {
+            get { return GetValue(() => Verbose); }
+            set { SetValue(() => Verbose, value); }
+        }
+
+        public int KmeansMaxIters
+        {
+            get { return GetValue(() => KmeansMaxIters); }
+            set { SetValue(() => KmeansMaxIters, value); }
+        }
     }
 }
