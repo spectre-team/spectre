6138e3a1c097aa1c3c7e8104d055fb8accd0b2e4
Regis128 <regis1286@gmail.com>
2017-03-25 08:58:17 +0100

    Get basic fields

--- src/Spectre.Algorithms/Results/DivikResult.cs
+++ src/Spectre.Algorithms/Results/DivikResult.cs
@@ -18,6 +18,9 @@
 */
 
 
+using System;
+using MathWorks.MATLAB.NET.Arrays.native;
+
 namespace Spectre.Algorithms.Results
 {
 	/// <summary>
@@ -25,15 +28,46 @@ namespace Spectre.Algorithms.Results
 	/// </summary>
 	public class DivikResult
     {
-	    private object _matlabResultStruct;
+        private object _matlabResultStruct;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="DivikResult"/> class.
-		/// </summary>
-		/// <param name="matlabResult">The results coming from MCR.</param>
-		internal DivikResult(object[] matlabResult)
+        /// <summary>
+        /// result[0]
+        /// </summary>
+        private Double[,] _mz;
+
+        /// <summary>
+        /// index from matlabResult[1]
+        /// </summary>
+        private Double[,] index;
+
+        /// <summary>
+        /// centoid from matlabResult[1]
+        /// </summary>
+        private Double[,] centoid;
+
+        /// <summary>
+        /// partition from matlabResult[1]
+        /// </summary>
+        private Double[,] partition;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DivikResult"/> class.
+        /// </summary>
+        /// <param name="matlabResult">The results coming from MCR.</param>
+        internal DivikResult(object[] matlabResult)
         {
 	        _matlabResultStruct = matlabResult[1];
+
+            object result = _matlabResultStruct;
+
+            _mz = (Double[,])matlabResult[0];
+
+            MWStructArray array = (MWStructArray)_matlabResultStruct;
+
+            centoid = (Double[,])array.GetField("centroids");
+            partition = (Double[,])array.GetField("partition");
+            index = (Double[,])array.GetField("index");
+            
         }
     }
 }
