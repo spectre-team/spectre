cc47cf7aa2ada89c5e5463f3a5903ecff4768417
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-04-18 12:16:08 +0200

    Fix parameter list error, add right-click call
    Allows to run the script with right-click: Run in powershell. Automatically
    prompts for elevated permissions.

--- scripts/BindPageToIisAndCreateDataDir.ps1
+++ scripts/BindPageToIisAndCreateDataDir.ps1
@@ -1,7 +1,9 @@
 Param(
-    [switch]$AppVeyor
+    [switch]$AppVeyor,
     [switch]$Verbose
 )
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 if($AppVeyor -eq $True)
 {
@@ -10,12 +12,14 @@ if($AppVeyor -eq $True)
 }
 else
 {
-    $ProjectRoot = Get-Location | Select-String -Pattern spectre -SimpleMatch
+    $ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+    $ProjectRoot = Split-Path -Parent $ScriptPath
+    #$ProjectRoot = Get-Location | Select-String -Pattern spectre -SimpleMatch
 }
 
 function VerbosePrint($text)
 {
-    if($Verbose)
+    if($Verbose -eq $True)
     {
         Write-Host $text
     }
@@ -27,8 +31,8 @@ Try
 {
     VerbosePrint -text "Adding Spectre application pool."
     Import-Module "WebAdministration"
-    New-Item 'IIS:\Sites\Default Web Site\spectre_api' -physicalPath "$($ProjectRoot)\src\Spectre" -type Application
-    New-Item 'IIS:\AppPools\Spectre'
+    New-Item 'IIS:\Sites\Default Web Site\spectre_api' -physicalPath "$($ProjectRoot)\src\Spectre" -type Application | Out-Null
+    New-Item 'IIS:\AppPools\Spectre' | Out-Null
     Set-ItemProperty 'IIS:\AppPools\Spectre' -Name managedRuntimeVersion -Value 'v4.0'
     Set-ItemProperty 'IIS:\AppPools\Spectre' -Name managedPipelineMode -Value 'Integrated'
     VerbosePrint -text "Adding $($ProjectRoot)\src\Spectre as an application."
@@ -45,16 +49,26 @@ Catch
 
 Try
 {
-    New-Item c:\spectre_data -type Directory
+    New-Item c:\spectre_data -type Directory | Out-Null
     $Acl = Get-Acl c:\spectre_data
     $Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("IIS_IUSRS", "FullControl", "Allow")
     $Acl.SetAccessRule($Ar)
     Set-Acl c:\spectre_data $Acl
-    Copy-Item "$($ProjectRoot)\test_files\*" c:\spectre_data
+    Copy-Item "$($ProjectRoot)\test_files\*" c:\spectre_data | Out-Null
 
     Write-Host "Test data copied to default data directory." -foregroundcolor green
 }
 Catch
 {
     Write-Host "Failed to copy data default directory." -foregroundcolor red
+}
+
+Try
+{
+    Invoke-WebRequest -Uri http://localhost/spectre_api/api/preparations/1?spectrumId=1 | Out-Null
+    Write-Host "API available, test data accessible." -foregroundcolor green
+}
+Catch
+{
+    Write-Host "Api is not available." -foregroundcolor red
 }
