37e5e1ceabb0112540728ecae5dfe4baf392c53b
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-05-09 14:39:12 +0200

    Add tests for Preparations controller
    Adds missing tests to check, wheter sample data are returned properly.
    Fixes problem with wrong intensity array returned for a spectrum.

---
+++ src/Spectre.Tests/Controllers/PreparationsControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Spectre.Controllers;
+using Spectre.Models.Msi;
+
+namespace Spectre.Tests.Controllers
+{
+    [TestFixture]
+    public class PreparationsControllerTest
+    {
+        private PreparationsController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new PreparationsController();
+        }
+
+        [Test]
+        public void TestGetListOfPreparations()
+        {
+            var list = _controller.Get();
+
+            Assert.NotNull(list);
+            Assert.IsNotEmpty(list);
+            Assert.AreEqual(1, list.Count());
+            Assert.AreEqual("Head & neck cancer, patient 1, tumor region only", list.First().Name);
+        }
+
+        [Test]
+        public void TestGetFirstPreparation()
+        {
+            var first = _controller.Get(1);
+
+            Assert.NotNull(first);
+            Assert.IsInstanceOf<Preparation>(first);
+            Assert.AreEqual(1, first.Id);
+            Assert.AreEqual("Head & neck cancer, patient 1, tumor region only", first.Name);
+        }
+
+        [Test]
+        public void TestGetFirstPreparationSampleSpectrum()
+        {
+            var spectrum = _controller.Get(1, 1);
+
+            Assert.NotNull(spectrum);
+            Assert.IsInstanceOf<Spectrum>(spectrum);
+            Assert.IsNotEmpty(spectrum.Intensities);
+            Assert.IsNotEmpty(spectrum.Mz);
+            Assert.AreEqual(spectrum.Mz.Count(), spectrum.Intensities.Count(), "Intensities and Mz counts do not match.");
+        }
+    }
+}
--- src/Spectre/Models/Msi/Preparation.cs
+++ src/Spectre/Models/Msi/Preparation.cs
@@ -2,14 +2,34 @@
 
 namespace Spectre.Models.Msi
 {
+    /// <summary>
+    /// Exhibits general preparation data to API.
+    /// </summary>
     [DataContract]
     public class Preparation
     {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         [DataMember]
         public string Name { get; private set; }
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         [DataMember]
         public int Id { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Preparation"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="id">The identifier.</param>
         public Preparation(string name, int id)
         {
             Name = name;
