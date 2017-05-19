8a92d1446d8049acc35b1e52bcb112d0b04804e9
Michał 'Sand3r' Gallus <sand3r@interia.eu>
2017-05-19 23:34:07 +0200

    Update copyright note at PreparationControllerTest

--- src/Spectre.Tests/Controllers/PreparationsControllerTest.cs
+++ src/Spectre.Tests/Controllers/PreparationsControllerTest.cs
@@ -2,7 +2,7 @@
  * PreparationsControllerTest.cs
  * Test for proper responses after requests.
  * 
-   Copyright 2017 Grzegorz Mrukwa
+   Copyright 2017 Grzegorz Mrukwa, Michał Gallus
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
