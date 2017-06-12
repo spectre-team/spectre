70fb5c7a3b7cd32aab76fe0088b9ed58ce58583e
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-06-12 07:53:30 +0200

    Add work-version of plotly component
    Needs fix for Karma tests

---
+++ src/Spectre.Angular2Client/src/app/plotly/plotly.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlotlyComponent } from './plotly.component';
+
+describe('PlotlyComponent', () => {
+  let component: PlotlyComponent;
+  let fixture: ComponentFixture<PlotlyComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PlotlyComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlotlyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
