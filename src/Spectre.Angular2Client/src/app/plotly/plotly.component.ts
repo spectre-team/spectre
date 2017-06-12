dfebd1a30105f09ce5fa7aad77a77fa62d9ef780
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-06-12 07:53:30 +0200

    Add work-version of plotly component
    Needs fix for Karma tests

---
+++ src/Spectre.Angular2Client/src/app/plotly/plotly.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+declare var Plotly: any;
+
+@Component({
+  selector: 'app-plotly',
+  templateUrl: './plotly.component.html',
+  styleUrls: ['./plotly.component.css']
+})
+export class PlotlyComponent implements OnInit {
+
+  @Input() data: any;
+  @Input() layout: any;
+  @Input() options: any;
+  @Input() displayRawData: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    	Plotly.plot('plotlyDiv',  [{
+    	x: [1, 2, 3, 4, 5],
+    	y: [1, 2, 4, 8, 16] }], {
+    	margin: { t: 0 } } );
+  }
+
+}
