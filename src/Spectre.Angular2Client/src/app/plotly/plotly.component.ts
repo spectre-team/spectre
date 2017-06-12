21123009c0204188fd61219d52cd8d99213101ff
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-06-12 11:17:18 +0200

    Fix Jasmine tests
    Adds modified version of Plotly package. Old one redefined polyfills and
    there were problems with Zone.js. Current build has modified es6 include
    as described here: https://github.com/plotly/plotly.js/issues/955
    
    Plotly component has to be further bind to data, but now it has just
    sample ones.

--- src/Spectre.Angular2Client/src/app/plotly/plotly.component.ts
+++ src/Spectre.Angular2Client/src/app/plotly/plotly.component.ts
@@ -17,10 +17,10 @@ export class PlotlyComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    	Plotly.plot('plotlyDiv',  [{
-    	x: [1, 2, 3, 4, 5],
-    	y: [1, 2, 4, 8, 16] }], {
-    	margin: { t: 0 } } );
+    Plotly.plot('plotlyDiv',  [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16] }], {
+    margin: { t: 0 } } );
   }
 
 }
