eb4b72aa5baef246a177fc078ffa0639bf7550e6
Michał 'Sand3r' Gallus <sand3r@interia.eu>
2017-07-11 17:34:16 +0200

    Give ownership capabilities to GaussianMixtureModel

--- src/Spectre.libGmmModel/GaussianMixtureModel.h
+++ src/Spectre.libGmmModel/GaussianMixtureModel.h
@@ -18,65 +18,87 @@
 */
 
 #pragma once
+#include <vector>
 #include <gsl.h>
 
-namespace Spectre::libGmmModel
+namespace Spectre::libGaussianMixtureModelling
 {
-	/// <summary>
-	/// Represents a single component of a Gaussian Mixture
-	/// </summary>
-	struct GaussianComponent
-	{
-		/// <summary>
-		/// Mean value of the component or peak's location
-		/// </summary>
-		double mean;
+    /// <summary>
+    /// Represents a single component of a Gaussian Mixture.
+    /// </summary>
+    struct GaussianComponent
+    {
+        /// <summary>
+        /// Mean value of the component or peak's location.
+        /// </summary>
+        double mean;
 
-		/// <summary>
-		/// Standard deviation of the component or peak's width
-		/// </summary>
-		double deviation;
+        /// <summary>
+        /// Standard deviation of the component or peak's width.
+        /// </summary>
+        double deviation;
 
-		/// <summary>
-		/// Weight of the component or peak's height
-		/// </summary>
-		double weight;
-	};
+        /// <summary>
+        /// Weight of the component or peak's height.
+        /// </summary>
+        double weight;
+    };
 
-	/// <summary>
-	/// Represents a composite of gaussian distribution components 
-	/// that build up a Gaussian Mixture Model.
-	/// </summary>
-	struct GaussianMixtureModel
-	{
-		/// <summary>
-		/// Collection of Gaussian components
-		/// </summary>
-		gsl::span<GaussianComponent> components;
+    /// <summary>
+    /// Represents a composite of gaussian distribution components 
+    /// that build up a Gaussian Mixture Model.
+    /// </summary>
+    struct GaussianMixtureModel
+    {
+        /// <summary>
+        /// Constructor used for initialization of m/z, intesities and gaussian components
+        /// collections.
+        /// </summary>
+        /// <param name="mzArray">M/z data shared by all spectra.</param>
+        /// <param name="intensities">Mean intensities at each point.</param>
+        /// <param name="numberOfComponents">Number of Gaussian Components to be set.</param>
+        GaussianMixtureModel(const std::vector<double>& mzArray,
+                             const std::vector<double>& intensities,
+                             unsigned int numberOfComponents) :
+            m_OriginalMzArray(mzArray), m_OriginalMeanSpectrum(intensities),
+            m_Components(numberOfComponents), components(m_Components),
+            originalMzArray(m_OriginalMzArray), originalMeanSpectrum(m_OriginalMeanSpectrum)
+        {
+        }
 
-		/// <summary>
-		/// Collection of initially supplied mz values
-		/// </summary>
-		gsl::span<double> originalMzArray;
+        /// <summary>
+        /// Collection of Gaussian components.
+        /// </summary>
+        gsl::span<GaussianComponent> components;
 
-		/// <summary>
-		/// Collection of average supplied spectra.
-		/// </summary>
-		gsl::span<double> originalMeanSpectrum;
+        /// <summary>
+        /// Collection of initially supplied mz values.
+        /// </summary>
+        gsl::span<double> originalMzArray;
 
-		/// <summary>
-		/// M/z threshold used in components merging.
-		/// </summary>
-		double mzMergingThreshold;
+        /// <summary>
+        /// Collection of average supplied spectra.
+        /// </summary>
+        gsl::span<double> originalMeanSpectrum;
 
-		/// <summary>
-		/// Value indicating whether this instance is merged.
-		/// </summary>
-		bool isMerged;
+        /// <summary>
+        /// M/z threshold used in components merging.
+        /// </summary>
+        double mzMergingThreshold;
 
-		/// <summary>
-		/// Gets a value indicating whether this instance is noise components reduced.
-		/// </summary>
-		bool isNoiseReduced;
-	};
+        /// <summary>
+        /// Value indicating whether this instance is merged.
+        /// </summary>
+        bool isMerged;
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is noise components reduced.
+        /// </summary>
+        bool isNoiseReduced;
+
+    private:
+        std::vector<double> m_OriginalMzArray;
+        std::vector<double> m_OriginalMeanSpectrum;
+        std::vector<GaussianComponent> m_Components;
+    };
 }
