4baf18a56e92e78c25e1d8ce75a76d413cd82f62
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-07-19 00:25:22 +0200

    Add license headers

--- src/Spectre.libGenetic/ParentSelectionStrategy.cpp
+++ src/Spectre.libGenetic/ParentSelectionStrategy.cpp
@@ -1,3 +1,22 @@
+/*
+* ParentSelectionStrategy.cpp
+* Strategy describing how parents are chosen to create offspring.
+*
+Copyright 2017 Grzegorz Mrukwa
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include "ParentSelectionStrategy.h"
 
 namespace Spectre::libGenetic
@@ -15,5 +34,4 @@ std::pair<const Individual&, const Individual&> ParentSelectionStrategy::next(co
     const auto second = indexDistribution(m_RandomNumberGenerator);
     return std::make_pair(generation[first], generation[second]);
 }
-
 }
