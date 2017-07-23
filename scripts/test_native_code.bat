9e7d62b1ebe6830d48b2903877a95d1647cf8c59
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-07-23 14:23:05 +0200

    Add CI build failure on failed GTests

--- scripts/test_native_code.bat
+++ scripts/test_native_code.bat
@@ -5,8 +5,13 @@ dir C:\projects\spectre\*Tests.exe /b /s | findstr /v obj > __tmp_gtest.txt
 
 echo Testing (Google Test)...
 
+set failures=0
+
 FOR /F %%i IN (__tmp_gtest.txt) DO (
 	echo %%i
 	%%i
+	set /A failures=%failures%+1
 )
 del __tmp_gtest.txt
+
+EXIT /B 1
