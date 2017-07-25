a35feb81ab4ebe0ac84aca9b8cc86411336b879f
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-07-26 00:13:17 +0200

    Add Jasmine reporting

--- scripts/upload-frontend-test-results.bat
+++ scripts/upload-frontend-test-results.bat
@@ -3,3 +3,6 @@ cd C:\projects\spectre\src\Spectre.Angular2Client
 IF EXIST "C:\projects\spectre\src\Spectre.Angular2Client\karma-tests.xml" (
     powershell C:\projects\spectre\scripts\Upload-TestResult.ps1 -fileName C:\projects\spectre\src\Spectre.Angular2Client\karma-tests.xml
 )
+IF EXIST "C:\projects\spectre\src\Spectre.Angular2Client\protractor-tests.xml" (
+    powershell C:\projects\spectre\scripts\Upload-TestResult.ps1 -fileName C:\projects\spectre\src\Spectre.Angular2Client\protractor-tests.xml
+)
