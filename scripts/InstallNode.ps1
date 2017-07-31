c2b7a2b1dc29b63f450c754837ac553acb1d047d
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-07-31 19:06:51 +0200

    Restore Angular installation verbosity

--- scripts/InstallNode.ps1
+++ scripts/InstallNode.ps1
@@ -1,10 +1,10 @@
 Install-Product node 6.10.0 x64
 Write-Host "Installing npm..." -ForegroundColor Yellow
-npm install -g npm@4.4.1 | Out-Null
+npm install -g npm@4.4.1
 $env:path = $env:appdata + "\npm;" + $env:path
 Write-Host "Installing angular-cli..." -ForegroundColor Yellow
-npm install -g @angular/cli@1.0.0-rc.2 --loglevel=error | Out-Null
+npm install -g @angular/cli@1.0.0-rc.2 --loglevel=error
 cd .\src\Spectre.AngularClient
 Write-Host "Installing dependencies..." -ForegroundColor Yellow
-npm install --loglevel=error | Out-Null
+npm install --loglevel=error
 cd ..\..
