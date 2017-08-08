6697dcd6efcdd2678789935281b4f00b559701a8
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-08-08 23:27:13 +0200

    Add StyleCop, remove AnyCPU target, add docstrings

--- src/Spectre.Database/Entities/Dataset.cs
+++ src/Spectre.Database/Entities/Dataset.cs
@@ -1,21 +1,91 @@
-﻿using System;
+﻿/*
+ * Dataset.cs
+ * Database model of dataset metadata
+ *
+   Copyright 2017 Roman Lisak
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Spectre.Database.Entities
 {
+    /// <summary>
+    /// Abstraction of dataset metadata needed to load a dataset
+    /// </summary>
     public class Dataset
     {
+        /// <summary>
+        /// Gets or sets the item identifier.
+        /// </summary>
+        /// <value>
+        /// The item identifier.
+        /// </value>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ItemId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hash.
+        /// </summary>
+        /// <value>
+        /// The hash.
+        /// </value>
         [Index]
-        public string hash { get; set; }
+        public string Hash { get; set; }
+
+        /// <summary>
+        /// Gets or sets the friendly name.
+        /// </summary>
+        /// <value>
+        /// The friendly name.
+        /// </value>
         [Index]
         public string FriendlyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the owner.
+        /// </summary>
+        /// <value>
+        /// The owner.
+        /// </value>
         public string Owner { get; set; }
+
+        /// <summary>
+        /// Gets or sets the upload time.
+        /// </summary>
+        /// <value>
+        /// The upload time.
+        /// </value>
         public DateTime UploadTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location on the disk.
+        /// </summary>
+        /// <value>
+        /// The location on the disk.
+        /// </value>
         public string Location { get; set; }
     }
 }
--- src/Spectre.Database/Migrations/Configuration.cs
+++ src/Spectre.Database/Migrations/Configuration.cs
@@ -1,9 +1,35 @@
-﻿using System.Data.Entity.Migrations;
+﻿/*
+ * Configuration.cs
+ * Configuration which creates automatic migrations
+ *
+   Copyright 2017 Roman Lisak
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Data.Entity.Migrations;
 
 namespace Spectre.Database.Migrations
 {
+    /// <summary>
+    /// Configuration of dataset migrations.
+    /// </summary>
+    /// <seealso cref="Utils.Context" />
     internal sealed class Configuration : DbMigrationsConfiguration<Utils.Context>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Configuration"/> class.
+        /// </summary>
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
