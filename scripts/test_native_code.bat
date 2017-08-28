3e8fbd99451bedb3af621ec6487207a7ac024b5b
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-08-28 17:39:43 +0200

    Solve problem of non-failing native tests

--- scripts/test_native_code.bat
+++ scripts/test_native_code.bat
@@ -10,10 +10,10 @@ set failures=0
 FOR /F %%i IN (__tmp_gtest.txt) DO (
 	echo %%i
 	%%i --gtest_output="xml:%%i.xml"
-	powershell C:\projects\spectre\scripts\Upload-TestResult.ps1 -fileName %%i.xml
 	IF %ERRORLEVEL% NEQ 0 (
 		set /A failures=%failures%+1
 	)
+	powershell C:\projects\spectre\scripts\Upload-TestResult.ps1 -fileName %%i.xml
 )
 del __tmp_gtest.txt
 
