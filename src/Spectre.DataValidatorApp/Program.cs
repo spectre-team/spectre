59206d057a749aefacf47cee8e71c8562e6b35ff
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-10-24 10:18:22 +0200

    Introduce dataset validation application

---
+++ src/Spectre.DataValidatorApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Spectre.Data.Datasets;
+
+namespace Spectre.DataValidatorApp
+{
+    /// <summary>
+    /// This application purpose is to validate input data files.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entry point of the program.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Write(value: $"Usage: {AppDomain.CurrentDomain.FriendlyName} path_to_data_file");
+            }
+            else
+            {
+                try
+                {
+                    new BasicTextDataset(textFilePath: args[0]);
+                    Console.WriteLine(value: "Validation success.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine(value: "Validation failure.");
+                }
+            }
+        }
+    }
+}
