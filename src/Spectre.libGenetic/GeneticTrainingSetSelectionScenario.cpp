5fc8566003ce3640c29fcb6cff712c85f24c277a
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-10-25 19:37:31 +0200

    Fix indents in GeneticTrainingSetSelectionScenario

--- src/Spectre.libGenetic/GeneticTrainingSetSelectionScenario.cpp
+++ src/Spectre.libGenetic/GeneticTrainingSetSelectionScenario.cpp
@@ -3,10 +3,13 @@
 * Executes whole genetic algorithm and returns results
 *
 Copyright 2017 Spectre Team
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
 http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,15 +30,15 @@ namespace Spectre::libGenetic
 {
 
     GeneticTrainingSetSelectionScenario::GeneticTrainingSetSelectionScenario(double trainingRate,
-        double mutationRate,
-        double bitSwapRate,
-        double preservationRate,
-        unsigned int generationAmount,
-        const std::vector<unsigned int>& generationSize,
-        const std::vector<unsigned int>& trueAmount,
-        const std::string& filename,
-        unsigned int runAmount,
-        Seed seed) :
+                                                                             double mutationRate,
+                                                                             double bitSwapRate,
+                                                                             double preservationRate,
+                                                                             unsigned int generationAmount,
+                                                                             const std::vector<unsigned int>& generationSize,
+                                                                             const std::vector<unsigned int>& trueAmount,
+                                                                             const std::string& filename,
+                                                                             unsigned int runAmount,
+                                                                             Seed seed) :
         m_GenerationSizes(generationSize.begin(), generationSize.end()),
         m_InitialIndividualFillups(trueAmount.begin(), trueAmount.end()),
         m_TrainingDatasetSizeRate(trainingRate),
