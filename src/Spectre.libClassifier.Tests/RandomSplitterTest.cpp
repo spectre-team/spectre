af663d963153f88f69dcf4f8e28c21f1db86fc8e
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-10-28 23:47:30 +0200

    Introduce the test that checks for the bug
    To indicate the problem with the splitter.

--- src/Spectre.libClassifier.Tests/RandomSplitterTest.cpp
+++ src/Spectre.libClassifier.Tests/RandomSplitterTest.cpp
@@ -36,11 +36,16 @@ class RandomSplitterTest : public ::testing::Test
 {
 public:
     RandomSplitterTest()
-        :randomSplitter(training, seed) {}
+        :dataset(data, labels),
+         randomSplitter(training, seed) {}
 
 protected:
     const Spectre::libGenetic::Seed seed = 1;
     const double training = 0.7;
+    const std::vector<DataType> data{ 0.5f, 0.4f, 0.6f, 1.1f, 1.6f, 0.7f, 2.1f, 1.0f, 0.9f, 0.8f };
+    // @gmrukwa: We're assuming labels are unique in tests below
+    const std::vector<Label> labels{ 3, 7, 14, 2, 12, 5, 8, 4, 19, 11 };
+    const OpenCvDataset dataset;
     RandomSplitter randomSplitter;
 
     void SetUp() override
@@ -51,21 +56,30 @@ protected:
 
 TEST_F(RandomSplitterTest, split_dataset)
 {
-    const std::vector<DataType> data{ 0.5f, 0.4f, 0.6f, 1.1f, 1.6f, 0.7f, 2.1f, 1.0f, 0.9f, 0.8f };
-    const std::vector<Label> labels{ 3, 7, 14, 2, 12, 5, 8, 4, 19, 11 };
-    OpenCvDataset dataset(data, labels);
-    SplittedOpenCvDataset result = randomSplitter.split(dataset);
+    const auto result = randomSplitter.split(dataset);
     EXPECT_EQ(result.trainingSet.size() + result.testSet.size(), labels.size());
 }
 
 TEST_F(RandomSplitterTest, check_size_of_splitted_vectors)
 {
-    const std::vector<DataType> data{ 0.5f, 0.4f, 0.6f, 1.1f, 1.6f, 0.7f, 2.1f, 1.0f, 0.9f, 0.8f };
-    const std::vector<Label> labels{ 3, 7, 14, 2, 12, 5, 8, 4, 19, 11 };
-    OpenCvDataset dataset(data, labels);
     const auto result = randomSplitter.split(dataset);
     EXPECT_EQ(result.trainingSet.size(), data.size()*training);
     EXPECT_EQ(result.testSet.size(), data.size()- data.size()*training);
 }
 
+TEST_F(RandomSplitterTest, splits_differently_for_different_seeds)
+{
+    randomSplitter = RandomSplitter(training, seed);
+    const auto first = randomSplitter.split(dataset);
+    randomSplitter = RandomSplitter(training, seed + 1);
+    const auto second = randomSplitter.split(dataset);
+    for (auto i = 0u; i < first.trainingSet.size(); ++i)
+    {
+        if(first.trainingSet.GetSampleMetadata(i) != second.trainingSet.GetSampleMetadata(i))
+        {
+            SUCCEED();
+        }
+    }
+    FAIL() << "Outputs for different seeds are exactly the same.";
+}
 }
