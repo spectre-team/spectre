f959d1aedcd2304d532cca41bc9e837d82b7c1b0
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-11-08 12:06:04 +0100

    Add converter application

---
+++ src/Spectre.DivikResultConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectre.DivikResultConverter
+{
+    /// <summary>
+    /// Entry point class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
