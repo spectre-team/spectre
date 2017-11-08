a1d3ca1e9c51eb133b9297b01573eb11c51b0424
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-11-08 11:55:00 +0100

    Add test placeholder

---
+++ src/Spectre.Algorithms.Tests/Io/DivikResultLoaderTests.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * DivikResultLoaderTests.cs
+ * Tests loading and parsing DiviK results from MAT-files.
+ *
+   Copyright 2017 Spectre Team
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using NUnit.Framework;
+
+namespace Spectre.Algorithms.Tests.Io
+{
+    [TestFixture]
+    [Category(name: "Algorithm")]
+    public class DivikResultLoaderTests
+    {
+        [Test]
+        public void CreateTestsForThisClass()
+        {
+            Assert.Fail();
+        }
+    }
+}
