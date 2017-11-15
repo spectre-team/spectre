89b8a4353523b362724fc7f29cef2e84e7a0f686
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-11-15 01:36:06 +0100

    Introduce CohenEffectSize
    First implementation of ValuesHomogeneityEstimator.

---
+++ src/Spectre.libStatistics/CohenEffectSize.cpp
@@ -0,0 +1,74 @@
+/*
+* CohenEffectSize.cpp
+* Estimates effect size.
+*
+Copyright 2017 Spectre Team
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "Spectre.libException/ReachedUnreachableCodeException.h"
+#include "Spectre.libStatistics/Statistics.h"
+#include "Spectre.libStatistics/Math.h"
+#include "CohenEffectSize.h"
+#include "Spectre.libException/EmptyArgumentException.h"
+
+namespace Spectre::libStatistics::statistical_testing
+{
+const std::array<std::string, 5> CohenEffectSize::interpretations = {
+    std::string("None"),
+    "Small",
+    "Medium",
+    "Large",
+    "Very Large"
+};
+
+const std::array<PrecisionType, 5> CohenEffectSize::thresholds = {
+    -std::numeric_limits<PrecisionType>::infinity(),
+    0.3,
+    0.5,
+    0.8,
+    1.2
+};
+
+using namespace simple_statistics;
+using namespace basic_math;
+
+StatisticalIndex CohenEffectSize::Compare(Values first, Values second) const
+{
+    if (first.empty())
+    {
+        throw libException::EmptyArgumentException("first");
+    }
+    if (second.empty())
+    {
+        throw libException::EmptyArgumentException("second");
+    }
+    if (first.size() + second.size() == 2)
+    {
+        return StatisticalIndex(0.0, 0u, interpretations[0]);
+    }
+    const auto firstMean = Mean(first);
+    const auto secondMean = Mean(second);
+    const auto std = sqrt(Variance(first, second));
+    const auto DCohen = std::abs(Mean(first) - Mean(second)) / sqrt(Variance(first, second));
+    for (auto thresholdNumber = thresholds.size() - 1; thresholdNumber >= 0; --thresholdNumber)
+    {
+        if (DCohen >= thresholds[thresholdNumber])
+        {
+            return StatisticalIndex(DCohen, static_cast<unsigned>(thresholdNumber), interpretations[thresholdNumber]);
+        }
+    }
+    throw libException::ReachedUnreachableCodeException();
+}
+}
