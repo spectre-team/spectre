bd1f3c0cc60cc762d2f95a613c30763faaeb71fc
Sebaxp95 <sebastianpustelnik95@gmail.com>
2017-11-30 17:24:01 +0100

    change bit build folder

--- scripts/package_bit_client.bat
+++ scripts/package_bit_client.bat
@@ -1,5 +1,5 @@
 cd C:\projects\spectre
 if "%CONFIGURATION%" == "Release" (
-    7z a WebClient-Bit.zip "%APPVEYOR_BUILD_FOLDER%\src\Spectre.AngularClient\dist\*" >nul 2>&1
+    7z a WebClient-Bit.zip "%APPVEYOR_BUILD_FOLDER%\src\Spectre.AngularClient\bitDist\*" >nul 2>&1
 )
 
