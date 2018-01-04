03dd574e821596fedc13874243d8d8e17807b003
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-04 02:14:13 +0100

    Remove MATLAB code

--- src/MATLAB/DiviK/demo.m
+++
@@ -1,9 +0,0 @@
-load sample_data.mat
-
-addpath(genpath('./libs'));
-
-% data has observations in rows and gmm components in columns
-% xy has observations in rows, x in first column, y in second
-partition = divik(data, xy, 'Level', 2, ...
-    'PlotPartitions', true, ...
-    'PlotRecursively', true);
--- src/MATLAB/dependencies/rows_in.m
+++
@@ -1,7 +0,0 @@
-function found = rows_in(to_find, reference)
-%ROWS_IN checks which rows are present in control matrix
-%   found = ROWS_IN(to_find, reference)
-    
-    found = ismember(to_find, reference, 'rows');
-    
-end
