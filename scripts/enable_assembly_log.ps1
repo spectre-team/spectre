f2caf9143ba7ef47f04c08082d0b9a72bd2398a5
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-04 09:40:00 +0100

    Remove .NET WebAPI

--- scripts/enable_assembly_log.ps1
+++
@@ -1,5 +0,0 @@
-$registryPath = "HKLM:\Software\Microsoft\Fusion"
-$name = "EnableLog"
-$value = "1"
-New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD
-Write-Host "Assembly resolving logs enabled." -ForegroundColor Yellow
