de1c435b7a5824416b5e90311bb99090a171aa72
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-04 12:15:02 +0100

    Simplify build process

--- scripts/build_ng_client.bat
+++
@@ -1,8 +0,0 @@
-cd C:\projects\spectre\src\Spectre.AngularClient
-if "%1" == "Release" (
-    ng build -prod --aot=false >nul 2>&1
-) else ( if "%1" == "Bit" (
-    ng build -env=bit --aot=false >nul 2>&1
-)  else (
-    ng build >nul 2>&1
-) )
--- scripts/package_angular_client.bat
+++
@@ -1,2 +0,0 @@
-cd C:\projects\spectre
-7z a WebClient-%CONFIGURATION%.zip "%APPVEYOR_BUILD_FOLDER%\src\Spectre.AngularClient\dist\*" >nul 2>&1
