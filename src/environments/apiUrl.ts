fae848896bf204e6ee2afc82bf96c78c20f60881
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-09 22:31:18 +0100

    Update environments

--- src/environments/apiUrl.ts
+++ src/environments/apiUrl.ts
@@ -1,11 +1,11 @@
 export const apiUrl = {
-  spectrumByIdUrl: '/spectrum/%d?spectrumId=%d',
-  spectrumByCoordUrl: '/spectrum/%d?x=%d&y=%d',
-  preparationsUrl: '/preparations',
-  preparationUrl: '/preparations/%s',
-  heatmapUrl: '/heatmap/%s?channelId=%s&flag=false',
-  divikResultUrl: '/divikResult/%s?divikId=#d&level=#s',
-  divikConfigUrl: '/divikResult/%s?divikId=%s',
-  uploadUrl: '/download'
+  spectrumByIdUrl: ':2001/spectrum/%d?spectrumId=%d',
+  spectrumByCoordUrl: ':2001/spectrum/%d?x=%d&y=%d',
+  preparationsUrl: ':2001/preparations',
+  preparationUrl: ':2001/preparations/%s',
+  heatmapUrl: ':2001/heatmap/%s?channelId=%s&flag=false',
+  divikResultUrl: ':2001/divikResult/%s?divikId=#d&level=#s',
+  divikConfigUrl: ':2001/divikResult/%s?divikId=%s',
+  uploadUrl: ':2002/api/download'
 
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost/spectre_api'
+  apiUrl: 'http://spectre.westeurope.cloudapp.azure.com'
 };
