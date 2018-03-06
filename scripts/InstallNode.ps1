43700e46702f7e6c6386d7894ac57fdd72af7f20
daniel.babiak <daniel.babiak8@gmail.com>
2018-03-06 17:15:30 +0100

    Update Node.js, npm and Angular-CLI version in scripts

--- scripts/InstallNode.ps1
+++ scripts/InstallNode.ps1
@@ -1,8 +1,8 @@
-Install-Product node 6.10.0 x64
+Install-Product node 8.9.4 x64
 Write-Host "Installing npm..." -ForegroundColor Yellow
-npm install -g npm@4.4.1 | Out-Null
+npm install -g npm@5.6.0 | Out-Null
 $env:path = $env:appdata + "\npm;" + $env:path
 Write-Host "Installing angular-cli..." -ForegroundColor Yellow
-npm install -g @angular/cli@1.0.0-rc.2 --loglevel=error | Out-Null
+npm install -g @angular/cli@1.7.1 --loglevel=error | Out-Null
 Write-Host "Installing dependencies..." -ForegroundColor Yellow
 npm install --loglevel=error | Out-Null
