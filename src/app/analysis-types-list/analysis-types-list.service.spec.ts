01cf8e747059e883484be70e46e55a5f360cd6dc
Roman <roman.lisak62@gmail.com>
2018-03-18 16:12:47 +0100

    Fix tests

--- src/app/analysis-types-list/analysis-types-list.service.spec.ts
+++ src/app/analysis-types-list/analysis-types-list.service.spec.ts
@@ -24,6 +24,7 @@ import 'rxjs/add/observable/of';
 import {AnalysisTypesListService} from './analysis-types-list.service';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
+import {Service} from "../app.service";
 
 class MockHttpClient {
   get(url) {
@@ -41,7 +42,8 @@ describe('AnalysisTypesListService', () => {
     TestBed.configureTestingModule({
       providers: [
         {provide: HttpClient, useClass: MockHttpClient},
-        AnalysisTypesListService
+        AnalysisTypesListService,
+        Service
       ]
     });
   });
